88638a40f74a3930878b3fc5519f2e1019e08e24
talkad <talkad@post.bgu.ac.il>
2021-04-12 10:00:43 +0300

    added pre-processing

--- covid19_bloodTesting_pred/main.py
+++ covid19_bloodTesting_pred/main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import pandas as pd
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# 2a - performing pre-processing
+df = pd.read_excel("dataset.xlsx", engine="openpyxl")
+
+df = df[["SARS-Cov-2 exam result", "Hematocrit", "Hemoglobin", "Platelets", "Red blood Cells", "Lymphocytes",
+         "Mean corpuscular hemoglobin (MCH)", "Mean corpuscular hemoglobin concentration (MCHC)",
+         "Leukocytes", "Basophils", "Eosinophils", "Lactic Dehydrogenase", "Mean corpuscular volume (MCV)",
+         "Red blood cell distribution width (RDW)", "Monocytes", "Mean platelet volume ", "Neutrophils",
+         "Proteina C reativa mg/dL", "Creatinine", "Urea", "Potassium", "Sodium", "Aspartate transaminase",
+         "Alanine transaminase"]]
+
+df.dropna(thresh=23*0.05, inplace=True)  # filtering rows with more than one empty cell (~95% threshold)
+
+covid_results = df["SARS-Cov-2 exam result"]
+df.drop(columns=["SARS-Cov-2 exam result"], inplace=True)
+
+imputer = IterativeImputer(max_iter=250)
+imputer.fit(df)
+imputed_df = imputer.transform(df)
+imputed_df = pd.DataFrame(imputed_df, columns=df.columns)
+
+binary_labels = covid_results.apply(lambda res: 0 if res == 'negative' else 1)
+
+# filtered_df.to_excel("asd.xlsx", engine="openpyxl")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 2b
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
