394e83917b6c55bcc6e67b0c697db17b32a04c1b
talkad <talkad@post.bgu.ac.il>
2021-05-11 14:47:29 +0300

    some progress

--- cascading_metaLearner/main.py
+++ cascading_metaLearner/main.py
@@ -1,11 +1,9 @@
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.datasets import load_digits
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss
 import pandas as pd
 import numpy as np
 
-improvement = True
+improvement = False
 
 
 def train(X2, y2):
@@ -44,12 +42,18 @@ if __name__ == '__main__':
     threshold = 0.95
     test_ration = 0.1
 
-    # digits = load_digits()
+    # filename = 'frogs_MFCCs.csv'
     #
-    # X_train, X_test, y_train, y_test = train_test_split(digits.images, digits.target, test_size=test_ration)
+    # df = pd.read_csv(filename)
+    # df = df.sample(frac=1).reset_index(drop=True)
+    # df[['Family']] = df[['Family']].apply(lambda col: pd.Categorical(col).codes)
+    # y = df['Family']
+    # df.drop(columns=['Family', 'Genus', 'Species', 'RecordID'], inplace=True)
     #
-    # X_train = [X.flatten() for X in X_train]
-    # X_test = [X.flatten() for X in X_test]
+    # X = df.to_numpy()
+    # total = len(X)
+    #
+    # X_train, X_test, y_train, y_test = X[:int(total*(1-test_ration))], X[int(total*(1-test_ration)):], y[:int(total*(1-test_ration))], y[int(total*(1-test_ration)):]
     #
     # # cascade classifiers training
     # models = train(X_train, y_train)
@@ -61,13 +65,70 @@ if __name__ == '__main__':
     #     _, probability = predict(models, X_test[i])
     #     y_hat.append(probability)
     #
-    # print(f'log loss = {log_loss(y_test, y_hat)}')
+    # print(f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
 
-    df = pd.read_csv('frogs_MFCCs.csv')
+
+    # filename = 'Dry_Bean_Dataset.xlsx'
+    #
+    # df = pd.read_excel(filename)
+    # df = df.sample(frac=1).reset_index(drop=True)
+    # df[['Class']] = df[['Class']].apply(lambda col: pd.Categorical(col).codes)
+    # y = df['Class']
+    #
+    # X = df.to_numpy()
+    # total = len(X)
+    #
+    # X_train, X_test, y_train, y_test = X[:int(total*(1-test_ration))], X[int(total*(1-test_ration)):], y[:int(total*(1-test_ration))], y[int(total*(1-test_ration)):]
+    #
+    # # cascade classifiers training
+    # models = train(X_train, y_train)
+    #
+    # # evaluate model
+    # y_hat = list()
+    #
+    # for i in range(len(y_test)):
+    #     _, probability = predict(models, X_test[i])
+    #     y_hat.append(probability)
+    #
+    # print(f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
+
+
+    # filename = 'winequality-red.csv'
+    #
+    # df = pd.read_csv(filename)
+    # df = df.sample(frac=1).reset_index(drop=True)
+    # df[['quality']] = df[['quality']].apply(lambda col: pd.Categorical(col).codes)
+    # y = df['quality']
+    #
+    # X = df.to_numpy()
+    # total = len(X)
+    #
+    # X_train, X_test, y_train, y_test = X[:int(total*(1-test_ration))], X[int(total*(1-test_ration)):], y[:int(total*(1-test_ration))], y[int(total*(1-test_ration)):]
+    #
+    # # cascade classifiers training
+    # models = train(X_train, y_train)
+    #
+    # print(set(y_test), set(y_train))
+    #
+    # # evaluate model
+    # y_hat = list()
+    #
+    # for i in range(len(y_test)):
+    #     _, probability = predict(models, X_test[i])
+    #     y_hat.append(probability)
+    #
+    # print(f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
+
+
+    filename = 'CTG.xls'
+
+    df = pd.read_excel(filename)
+    df.drop(columns=['b', 'e', 'AC', 'FM', 'UC', 'DL', 'DS', 'DP', 'DR'], inplace=True)
+
     df = df.sample(frac=1).reset_index(drop=True)
-    df[['Family']] = df[['Family']].apply(lambda col: pd.Categorical(col).codes)
-    y = df['Family']
-    df.drop(columns=['Family', 'Genus', 'Species', 'RecordID'], inplace=True)
+    df[['CLASS']] = df[['CLASS']].apply(lambda col: pd.Categorical(col).codes)
+    y = df['CLASS']
+    df.drop(columns=['CLASS'], inplace=True)
 
     X = df.to_numpy()
     total = len(X)
@@ -84,8 +145,4 @@ if __name__ == '__main__':
         _, probability = predict(models, X_test[i])
         y_hat.append(probability)
 
-    print(f'log loss = {log_loss(y_test, y_hat)}')
-
-
-
-
+    print(f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
