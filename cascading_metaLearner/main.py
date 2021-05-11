310cf058beda3b27527bbdb185a14eb84e08da13
talkad <talkad@post.bgu.ac.il>
2021-05-11 13:44:55 +0300

    added code for frogs dataset and digits

--- cascading_metaLearner/main.py
+++ cascading_metaLearner/main.py
@@ -1,15 +1,24 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.datasets import load_digits
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss
+import pandas as pd
+import numpy as np
+
+improvement = True
 
 
-def train():
+def train(X2, y2):
     model_list = list()
     for depth in range(2, 16):
+
+        if improvement and depth >= 3:
+            predictions = model_list[depth - 3].predict_proba(X2)
+            # append previous meta learner predictions probabilities to the train set
+            X2 = np.hstack((X2, predictions))
+
         model = DecisionTreeClassifier(max_depth=depth)
-        model.fit(X_train, y_train)
+        model.fit(X2, y2)
         model_list.append(model)
 
     return model_list
@@ -17,37 +26,56 @@ def train():
 
 def predict(model_list, x):
 
-    for index in range(2, 16):
+    for index in range(len(model_list)):
+
         model = model_list[index]
         pred = model.predict([x])
 
-        # print(f'iteration {index} with confidence {model.predict_proba([X_test[test]])[0][y_hat][0]}')
-
-        if model.predict_proba([x])[0][pred][0] > threshold:
-            # print(f'predicted class: {y_hat} with probability {model.predict_proba([X_test[test]])[0][y_hat][0]}')
+        if model.predict_proba([x])[0][pred] > threshold:
             return pred, model.predict_proba([x])[0]
+        elif improvement:
+            x = np.append(x, model.predict_proba([x])[0])
 
-    pred = models[-1].predict([x])
-    return pred, models[-1].predict_proba([x])[0]
+    pred = model_list[-1].predict([x])
+    return pred, model_list[-1].predict_proba([x])[0]
 
 
 if __name__ == '__main__':
     threshold = 0.95
+    test_ration = 0.1
 
-    digits = load_digits()
+    # digits = load_digits()
+    #
+    # X_train, X_test, y_train, y_test = train_test_split(digits.images, digits.target, test_size=test_ration)
+    #
+    # X_train = [X.flatten() for X in X_train]
+    # X_test = [X.flatten() for X in X_test]
+    #
+    # # cascade classifiers training
+    # models = train(X_train, y_train)
+    #
+    # # evaluate model
+    # y_hat = list()
+    #
+    # for i in range(len(y_test)):
+    #     _, probability = predict(models, X_test[i])
+    #     y_hat.append(probability)
+    #
+    # print(f'log loss = {log_loss(y_test, y_hat)}')
 
-    X_train, X_test, y_train, y_test = train_test_split(digits.images, digits.target, test_size=0.2)
+    df = pd.read_csv('frogs_MFCCs.csv')
+    df = df.sample(frac=1).reset_index(drop=True)
+    df[['Family']] = df[['Family']].apply(lambda col: pd.Categorical(col).codes)
+    y = df['Family']
+    df.drop(columns=['Family', 'Genus', 'Species', 'RecordID'], inplace=True)
 
-    plt.gray()
-    plt.matshow(X_test[256])
-    print(f'actual class: {y_test[256]}')
-    plt.show()
+    X = df.to_numpy()
+    total = len(X)
 
-    X_train = [X.flatten() for X in X_train]
-    X_test = [X.flatten() for X in X_test]
+    X_train, X_test, y_train, y_test = X[:int(total*(1-test_ration))], X[int(total*(1-test_ration)):], y[:int(total*(1-test_ration))], y[int(total*(1-test_ration)):]
 
     # cascade classifiers training
-    models = train()
+    models = train(X_train, y_train)
 
     # evaluate model
     y_hat = list()
@@ -58,5 +86,6 @@ if __name__ == '__main__':
 
     print(f'log loss = {log_loss(y_test, y_hat)}')
 
-    print(f'prediction: {predict(models, X_test[256])}')
+
+
 
