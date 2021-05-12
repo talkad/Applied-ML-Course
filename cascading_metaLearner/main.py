8094bfaabf96fe72a9d4c443dab265d255db26e4
talkad <talkad@post.bgu.ac.il>
2021-05-12 21:50:33 +0300

    finished assignment2

--- cascading_metaLearner/main.py
+++ cascading_metaLearner/main.py
@@ -5,27 +5,36 @@ from sklearn.metrics import log_loss
 import pandas as pd
 import numpy as np
 from lightgbm import LGBMClassifier
+import matplotlib.pyplot as plt
+import cifar10
 
-improvement = True
+improvement = False
 
 
 def train(X2, y2):
     model_list = list()
+    data = X2
+
     for depth in range(2, 16):
 
-        if improvement and depth >= 3:
-            predictions = model_list[depth - 3].predict_proba(X2)
-            # append previous meta learner predictions probabilities to the train set
-            X2 = np.hstack((X2, predictions))
+        if improvement:
+            model = LGBMClassifier(max_depth=depth)
+        else:
+            model = DecisionTreeClassifier(max_depth=depth)
 
-        model = DecisionTreeClassifier(max_depth=depth)
-        model.fit(X2, y2)
+        model.fit(data, y2)
         model_list.append(model)
+
+        if improvement:
+            prev_predictions = model_list[depth - 2].predict_proba(data)
+            data = np.hstack((X2, prev_predictions))
 
     return model_list
 
 
 def predict(model_list, x):
+
+    original_x = x
 
     for index in range(len(model_list)):
 
@@ -35,7 +44,7 @@ def predict(model_list, x):
         if model.predict_proba([x])[0][pred] > threshold:
             return pred, model.predict_proba([x])[0]
         elif improvement:
-            x = np.append(x, model.predict_proba([x])[0])
+            x = np.append(original_x, model.predict_proba([x])[0])
 
     pred = model_list[-1].predict([x])
     return pred, model_list[-1].predict_proba([x])[0]
@@ -100,6 +109,9 @@ if __name__ == '__main__':
     #
     # df = pd.read_csv(filename)
     # df = df.sample(frac=1).reset_index(drop=True)
+    # df = df[4 <= df.quality]
+    # df = df[df.quality <= 7]
+    #
     # df[['quality']] = df[['quality']].apply(lambda col: pd.Categorical(col).codes)
     # y = df['quality']
     #
@@ -111,8 +123,6 @@ if __name__ == '__main__':
     # # cascade classifiers training
     # models = train(X_train, y_train)
     #
-    # print(set(y_test), set(y_train))
-    #
     # # evaluate model
     # y_hat = list()
     #
@@ -122,31 +132,6 @@ if __name__ == '__main__':
     #
     # print(f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
 
-    # filename = 'hmnist.csv'
-    #
-    # df = pd.read_csv(filename)
-    # df = df.sample(frac=1).reset_index(drop=True)
-    # df[['label']] = df[['label']].apply(lambda col: pd.Categorical(col).codes)
-    # y = df['label']
-    #
-    # X = df.to_numpy()
-    # total = len(X)
-    #
-    # X_train, X_test, y_train, y_test = X[:int(total*(1-test_ration))], X[int(total*(1-test_ration)):], y[:int(total*(1-test_ration))], y[int(total*(1-test_ration)):]
-    #
-    # # cascade classifiers training
-    # models = train(X_train, y_train)
-    #
-    # print(set(y_test), set(y_train))
-    #
-    # # evaluate model
-    # y_hat = list()
-    #
-    # for i in range(len(y_test)):
-    #     _, probability = predict(models, X_test[i])
-    #     y_hat.append(probability)
-    #
-    # print(f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
 
     # filename = 'CTG.xls'
     #
@@ -175,33 +160,35 @@ if __name__ == '__main__':
     #
     # print(f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
 
-    filename = 'hmnist.csv'
 
-    df = pd.read_csv(filename)
-    df = df.sample(frac=1).reset_index(drop=True)
-    df[['label']] = df[['label']].apply(lambda col: pd.Categorical(col).codes)
-    y = df['label']
+    # filename = 'hmnist.csv'
+    #
+    # df = pd.read_csv(filename)
+    # df = df.sample(frac=1).reset_index(drop=True)
+    # df[['label']] = df[['label']].apply(lambda col: pd.Categorical(col).codes)
+    # y = df['label']
+    #
+    # X = df.to_numpy()
+    # total = len(X)
+    #
+    # X_train, X_test, y_train, y_test = X[:int(total * (1 - test_ration))], X[int(total * (1 - test_ration)):], y[:int(
+    #     total * (1 - test_ration))], y[int(total * (1 - test_ration)):]
+    #
+    # # cascade classifiers training
+    # models = train(X_train, y_train)
+    #
+    # print(set(y_test), set(y_train))
+    #
+    # # evaluate model
+    # y_hat = list()
+    #
+    # for i in range(len(y_test)):
+    #     _, probability = predict(models, X_test[i])
+    #     y_hat.append(probability)
+    #
+    # print(
+    #     f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
 
-    X = df.to_numpy()
-    total = len(X)
-
-    X_train, X_test, y_train, y_test = X[:int(total * (1 - test_ration))], X[int(total * (1 - test_ration)):], y[:int(
-        total * (1 - test_ration))], y[int(total * (1 - test_ration)):]
-
-    # cascade classifiers training
-    models = train(X_train, y_train)
-
-    print(set(y_test), set(y_train))
-
-    # evaluate model
-    y_hat = list()
-
-    for i in range(len(y_test)):
-        _, probability = predict(models, X_test[i])
-        y_hat.append(probability)
-
-    print(
-        f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
 
     # digits = load_digits()
     #
@@ -221,3 +208,107 @@ if __name__ == '__main__':
     #     y_hat.append(probability)
     #
     # print(f'model name: digits\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
+
+
+    # filename = 'letter-recognition.csv'
+    #
+    # df = pd.read_csv(filename)
+    # df = df.sample(frac=1).reset_index(drop=True)
+    # df[['class']] = df[['class']].apply(lambda col: pd.Categorical(col).codes)
+    # y = df['class']
+    # df.drop(columns=['class'], inplace=True)
+    #
+    # X = df.to_numpy()
+    # total = len(X)
+    #
+    # X_train, X_test, y_train, y_test = X[:int(total*(1-test_ration))], X[int(total*(1-test_ration)):], y[:int(total*(1-test_ration))], y[int(total*(1-test_ration)):]
+    #
+    # # cascade classifiers training
+    # models = train(X_train, y_train)
+    #
+    # # evaluate model
+    # y_hat = list()
+    #
+    # for i in range(len(y_test)):
+    #     _, probability = predict(models, X_test[i])
+    #     y_hat.append(probability)
+    #
+    # print(f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
+
+
+    # filename = 'page_block.xlsx'
+    #
+    # df = pd.read_excel(filename)
+    # df = df.sample(frac=1).reset_index(drop=True)
+    # df[['class']] = df[['class']].apply(lambda col: pd.Categorical(col).codes)
+    # y = df['class']
+    # df.drop(columns=['class'], inplace=True)
+    #
+    # X = df.to_numpy()
+    # total = len(X)
+    #
+    # X_train, X_test, y_train, y_test = X[:int(total*(1-test_ration))], X[int(total*(1-test_ration)):], y[:int(total*(1-test_ration))], y[int(total*(1-test_ration)):]
+    #
+    # # cascade classifiers training
+    # models = train(X_train, y_train)
+    #
+    # # evaluate model
+    # y_hat = list()
+    #
+    # for i in range(len(y_test)):
+    #     _, probability = predict(models, X_test[i])
+    #     y_hat.append(probability)
+    #
+    # print(f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
+
+
+    # filename = 'muscles.csv'
+    # df = pd.read_csv(filename)
+    #
+    # df = df.sample(frac=1).reset_index(drop=True)
+    # df[["class"]] = df[["class"]].apply(lambda col: pd.Categorical(col).codes)
+    # y = df['class']
+    # df.drop(columns=['class'], inplace=True)
+    #
+    # X = df.to_numpy()
+    # total = len(X)
+    #
+    # X_train, X_test, y_train, y_test = X[:int(total*(1-test_ration))], X[int(total*(1-test_ration)):], y[:int(total*(1-test_ration))], y[int(total*(1-test_ration)):]
+    #
+    # # cascade classifiers training
+    # models = train(X_train, y_train)
+    #
+    # # evaluate model
+    # y_hat = list()
+    #
+    # for i in range(len(y_test)):
+    #     _, probability = predict(models, X_test[i])
+    #     y_hat.append(probability)
+    #
+    # print(f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
+
+
+    filename = 'segmentation.csv'
+    df = pd.read_csv(filename)
+
+    df = df.sample(frac=1).reset_index(drop=True)
+    df[["REGIONCENTROIDCOL"]] = df[["REGIONCENTROIDCOL"]].apply(lambda col: pd.Categorical(col).codes)
+    y = df['REGIONCENTROIDCOL']
+    df.drop(columns=['REGIONCENTROIDCOL'], inplace=True)
+
+    X = df.to_numpy()
+    total = len(X)
+
+    X_train, X_test, y_train, y_test = X[:int(total*(1-test_ration))], X[int(total*(1-test_ration)):], y[:int(total*(1-test_ration))], y[int(total*(1-test_ration)):]
+
+    # cascade classifiers training
+    models = train(X_train, y_train)
+
+    # evaluate model
+    y_hat = list()
+
+    for i in range(len(y_test)):
+        _, probability = predict(models, X_test[i])
+        y_hat.append(probability)
+
+    print(f'model name: {filename}\n{"with improvements" if improvement else "no improvements"}\nlog loss = {log_loss(y_test, y_hat)}')
